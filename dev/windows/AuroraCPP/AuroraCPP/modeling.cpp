ffd9844dd4116336bd6a8050d65a4e21babf4791
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2020-06-18 23:57:25 -0700

    Initialized model

--- dev/windows/AuroraCPP/AuroraCPP/modeling.cpp
+++ dev/windows/AuroraCPP/AuroraCPP/modeling.cpp
@@ -1,2 +1,51 @@
 #pragma once
 #include "modeling.h"
+
+#pragma region functions
+
+#pragma region model
+void modelFwd(sPtr<cType> x, sPtr<cType> y) {
+	*y = *x;
+}
+void modelBwd(sPtr<cType> yGrad, sPtr<cType> xGrad) {
+	*xGrad = *yGrad;
+}
+#pragma endregion
+#pragma region bias
+void biasFwd(sPtr<cType> x, sPtr<cType> y) {
+
+}
+void biasBwd(sPtr<cType> x, sPtr<cType> y) {
+
+}
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region definitions
+
+#pragma region model
+void model::fwd() {
+	modelFwd(x, y);
+}
+void modelBpg::bwd() {
+	modelBwd(yGrad, xGrad);
+}
+void model::modelWise(function<void(model*)> func) {
+	func(this);
+}
+#pragma endregion
+#pragma region bias
+void bias::fwd() {
+	biasFwd(x, y);
+}
+void biasBpg::fwd() {
+	biasFwd(x, y);
+}
+void biasBpg::bwd() {
+	biasBwd(yGrad, xGrad);
+}
+#pragma endregion
+
+#pragma endregion
