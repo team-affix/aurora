27671f99f9aa1abf4e2e34266e5a5aab2b600c3b
teamaffixmb-pablo <66919907+teamaffixmb-pablo@users.noreply.github.com>
2020-07-12 15:38:44 -0700

    First Update
    This is the first update.

---
+++ dev/windows/AuroraRust/target/rls/debug/deps/AuroraRust-e1f7d28a8e6bb92d.d
@@ -0,0 +1,5 @@
+c:\Users\raigo\Desktop\CurrentProjects\RustDev\AuroraRust\target\rls\debug\deps\AuroraRust-e1f7d28a8e6bb92d.rmeta: src\main.rs
+
+c:\Users\raigo\Desktop\CurrentProjects\RustDev\AuroraRust\target\rls\debug\deps\AuroraRust-e1f7d28a8e6bb92d.d: src\main.rs
+
+src\main.rs:
