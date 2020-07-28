8d9530182ba06fb259433e9aa9e9c281a7f702c4
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2020-07-28 00:47:23 -0700

    Modified incrementing forward of recurrent models
    Prevents pointers of cTOut and hTOut to point to the same memory locations as
    cTIn and hTIn

--- dev/windows/AuroraCPP/AuroraCPP/general.h
+++ dev/windows/AuroraCPP/AuroraCPP/general.h
@@ -13,3 +13,30 @@ public:
 };
 
 void rep(function<void()> func, int iterations);
+
+template<class TIn, class TOut>
+vector<TOut> getVals(vector<TIn>* a, function<TOut(TIn)> getFrom) {
+	vector<TOut> result = vector<TOut>();
+	for (int i = 0; i < a->size(); i++) {
+		result.push_back(getFrom(a->at(i)));
+	}
+	return result;
+}
+
+template<class TIn, class TOut>
+void setVals(vector<TIn>* a, vector<TOut>* output, function<void(TIn&, TOut&)> setFrom) {
+
+	// make sure the input and output vectors are of the same size
+	assert(a->size() == output->size());
+
+	for (int i = 0; i < a->size(); i++) {
+		setFrom(a->at(i), output->at(i));
+	}
+}
+
+template<class T>
+void elemWise(vector<T>* a, function<void(T)> func) {
+	for (int i = 0; i < a->size(); i++) {
+		func(a->at(i));
+	}
+}
