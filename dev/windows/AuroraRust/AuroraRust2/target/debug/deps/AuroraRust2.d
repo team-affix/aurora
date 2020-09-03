8d1b9fc21014dc04591b996cf4a6b0c54a9450db
teamaffixmb-pablo <66919907+teamaffixmb-pablo@users.noreply.github.com>
2020-09-02 20:46:34 -0700

    Update

--- dev/windows/AuroraRust/AuroraRust2/target/debug/deps/AuroraRust2.d
+++ dev/windows/AuroraRust/AuroraRust2/target/debug/deps/AuroraRust2.d
@@ -1,14 +1,5 @@
-C:\Users\raigo\Documents\GitHub\aurora\dev\windows\AuroraRust\AuroraRust2\target\debug\deps\AuroraRust2.exe: src\main.rs src\weightJunction.rs src\weightSet.rs src\create.rs src\layer.rs src\model.rs src\math.rs src\bias.rs src\seq.rs src\act.rs
+C:\Users\raigo\Documents\GitHub\aurora\dev\windows\AuroraRust\AuroraRust2\target\debug\deps\AuroraRust2.exe: src\main.rs
 
-C:\Users\raigo\Documents\GitHub\aurora\dev\windows\AuroraRust\AuroraRust2\target\debug\deps\AuroraRust2.d: src\main.rs src\weightJunction.rs src\weightSet.rs src\create.rs src\layer.rs src\model.rs src\math.rs src\bias.rs src\seq.rs src\act.rs
+C:\Users\raigo\Documents\GitHub\aurora\dev\windows\AuroraRust\AuroraRust2\target\debug\deps\AuroraRust2.d: src\main.rs
 
 src\main.rs:
-src\weightJunction.rs:
-src\weightSet.rs:
-src\create.rs:
-src\layer.rs:
-src\model.rs:
-src\math.rs:
-src\bias.rs:
-src\seq.rs:
-src\act.rs:
