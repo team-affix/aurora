751555661c04b78d05d29d9b6571f9b979e47d4b
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2021-01-02 15:28:46 -0800

    Wiped Old Files

--- dev/windows/AuroraCPP/AuroraCPP/main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "superHeader.h"
-#include "maths.h"
-#include "modeling.h"
-#include "optimization.h"
-#include "processing.h"
-using namespace std;
--- dev/windows/AuroraCPP/AuroraCPP/processing.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "superHeader.h"
-#include "maths.h"
-using namespace std;
-
-vector<ptr<cType>> stride(ptr<cType> a, int kWidth, int kHeight, int horStride, int verStride);
